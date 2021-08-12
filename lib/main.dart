139846a31644e64452e7645b80be0356fb518241
Guglielmo Bergatto <69045457+GBergatto@users.noreply.github.com>
2021-08-12 16:53:35 +0200

    Add sign in and home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_authentication/authentication_service.dart';
+import 'package:firebase_authentication/screens/home_page.dart';
+import 'package:firebase_authentication/screens/sign_in_page.dart';
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,8 +16,20 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: AutenticationWrapper(),
+    return MultiProvider(
+      providers: [
+        Provider<AuthenticationService>(
+          create: (_) => AuthenticationService(FirebaseAuth.instance),
+        ),
+        StreamProvider(
+          initialData: null,
+          create: (context) =>
+              context.read<AuthenticationService>().authStateChanges,
+        ),
+      ],
+      child: MaterialApp(
+        home: AutenticationWrapper(),
+      ),
     );
   }
 }
@@ -20,6 +37,11 @@ class MyApp extends StatelessWidget {
 class AutenticationWrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final firebaseUser = context.watch<User>();
+
+    if (firebaseUser != null) {
+      return HomePage();
+    }
+    return SignInPage();
   }
 }
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../authentication_service.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("HOME"),
+          ],
+        ),
+      ),
+    );
+  }
+}
